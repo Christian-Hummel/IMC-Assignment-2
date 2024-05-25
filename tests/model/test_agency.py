84f0a033057bd39598d57cb43dd0dda4124d1322
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-25 21:27:47 +0200

    added first test case, throws RuntimeError

--- tests/model/test_agency.py
+++ tests/model/test_agency.py
@@ -1,22 +1,25 @@
 import pytest
 
 
-from ...src.model.supervisor import Supervisor
-from ...src.model.travelAgent import TravelAgent
+from src.model.supervisor import Supervisor
+from src.model.travelAgent import TravelAgent
 #from ...src.model.customer import Customer
 #from ...src.model.country import Country
-#from ...src.model.activity imoport Activity
+#from ...src.model.activity import Activity
 
-from ...src.database import Employee, Customer, Country, Activity,db
+from src.database import Employee, Customer, Country, Activity, db
 
 
 
-from ..fixtures import app, client, agency
+from tests.fixtures import app, client, agency
 
 
 def test_add_supervisor(agency):
-    new_supervisor = Supervisor(employee_id= 12, name="John Grisham", address="Penny Lane 23, 1255 Liverpool", salary=8000, nationality="England")
-    new_supervisor.email = "John.Grisham@hammertrips.com"
-    agency.add_supervisor(new_supervisor)
+        new_supervisor = Supervisor(employee_id= 12, name="John Grisham", address="Penny Lane 23, 1255 Liverpool", salary=8000, nationality="England")
+        new_supervisor.email = "John.Grisham@hammertrips.com"
+        agency.add_supervisor(new_supervisor)
 
-    print(db.session.scalars(db.select(Employee)).filter_by(employee_id=12))
+        print(db.session.scalars(db.select(Employee)).filter_by(employee_id=12))
+
+
+
