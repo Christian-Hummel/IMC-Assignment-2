fe88df6209f855905384d9cd5e2d23568766e002
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-28 09:03:50 +0200

    added test case for namespace

---
+++ tests/api/test_supervisorNS.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.database import db
+from tests.fixtures import app, client, agency
+
+def test_add_supervisor(client,agency):
+
+    response = client.post("/supervisor/", json={
+        "name":"Warren Buffet",
+        "address":"103 Avenue, 4932 New York",
+        "salary": 20000,
+        "nationality":"USA"
+    })
+
+
+    assert response.status_code == 200
+
+
+    parsed = response.get_json()
+    supervisor_response = parsed["supervisor"]
+
+
+    assert supervisor_response["name"] == "Warren Buffet"
