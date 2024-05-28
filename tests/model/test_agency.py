bf112559a7980ca839148753025b6537ecd68a4c
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-28 13:44:26 +0200

    added test case for add supervisor method

--- tests/model/test_agency.py
+++ tests/model/test_agency.py
@@ -1,11 +1,6 @@
 import pytest
 
 
-from src.model.supervisor import Supervisor
-from src.model.travelAgent import TravelAgent
-#from ...src.model.customer import Customer
-#from ...src.model.country import Country
-#from ...src.model.activity import Activity
 
 from src.database import Employee, Customer, Country, Activity, db
 
@@ -15,11 +10,23 @@ from tests.fixtures import app, client, agency
 
 
 def test_add_supervisor(agency):
-        new_supervisor = Supervisor(employee_id= 12, name="John Grisham", address="Penny Lane 23, 1255 Liverpool", salary=8000, nationality="England")
-        new_supervisor.email = "John.Grisham@hammertrips.com"
+        before = Employee.query.count()
+
+        new_supervisor = Employee(employee_id= 12, name="Terry Pratchett", address="Penny Lane 23, 1255 Liverpool",email="John.Grisham@hammertrips.com", salary=8000, nationality="England", role="supervisor")
         agency.add_supervisor(new_supervisor)
 
-        print(db.session.scalars(db.select(Employee)).filter_by(employee_id=12))
+        supervisor = db.session.query(Employee).filter_by(employee_id = 12).first()
+
+
+        assert Employee.query.count() == before + 1
+
+        assert supervisor.employee_id == 12
+        assert supervisor.name == "Terry Pratchett"
+        assert supervisor.address == "Penny Lane 23, 1255 Liverpool"
+        assert supervisor.email == "John.Grisham@hammertrips.com"
+        assert supervisor.salary == 8000
+        assert supervisor.nationality == "England"
+        assert supervisor.role == "supervisor"
 
 
 
