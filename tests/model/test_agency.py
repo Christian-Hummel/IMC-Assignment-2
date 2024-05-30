9fb4c374b8b55edc9a7f918913942267832abfa5
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-30 13:12:27 +0200

    added test case for user account registration for a supervisor in agency

--- tests/model/test_agency.py
+++ tests/model/test_agency.py
@@ -2,7 +2,7 @@ import pytest
 
 
 
-from src.database import Supervisor, TravelAgent, Customer, Country, Activity, db
+from src.database import Supervisor, TravelAgent, Customer, Country, Activity, User, db
 
 
 
@@ -20,13 +20,30 @@ def test_add_supervisor(agency):
 
         assert Supervisor.query.count() == before + 1
 
-        assert supervisor.employee_id == 12
+
         assert supervisor.name == "Terry Pratchett"
         assert supervisor.address == "Penny Lane 23, 1255 Liverpool"
         assert supervisor.email == "John.Grisham@hammertrips.com"
         assert supervisor.salary == 8000
         assert supervisor.nationality == "England"
         assert supervisor.role == "supervisor"
+
+
+def test_register_supervisor(agency):
+        before = User.query.count()
+
+        new_user = User(username="Franz",password_hash="AJLSJERFHS",id=15)
+
+        agency.register_user(new_user)
+
+        target_user = db.session.query(User).filter_by(id=new_user.id).first()
+
+        assert User.query.count() == before + 1
+
+        assert target_user.username == "Franz"
+        assert target_user.password_hash == "AJLSJERFHS"
+
+
 
 
 
