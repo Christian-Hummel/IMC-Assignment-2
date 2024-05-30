8e7521d56e0f6b7edd788770146fd13bcdae79f2
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-30 22:14:20 +0200

    added test case for adding a travel agent

--- tests/model/test_agency.py
+++ tests/model/test_agency.py
@@ -32,7 +32,7 @@ def test_add_supervisor(agency):
 def test_register_supervisor(agency):
         before = User.query.count()
 
-        new_user = User(username="Franz",password_hash="AJLSJERFHS",id=15)
+        new_user = User(username="Franz",password_hash="AJLSJERFHS")
 
         agency.register_user(new_user)
 
@@ -45,5 +45,20 @@ def test_register_supervisor(agency):
 
 
 
+def test_add_agent(agency):
+
+        before = TravelAgent.query.count()
+
+
+        new_agent = TravelAgent(employee_id=222,name="Sky DuMont",address="Alvaro 20,8573 Buenos Aires",email="Sky.DuMont@hammertrips.com",salary=3500,nationality="Germany",supervisor_id=135)
+
+        agency.add_travelagent(new_agent)
+
+        agent = db.session.query(TravelAgent).filter_by(employee_id=222).first()
+
+
+        assert TravelAgent.query.count() == before + 1
+
+        assert agent.name == "Sky DuMont"
 
 
