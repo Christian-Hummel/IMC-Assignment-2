7a2fd1958b197237e66331c07095e9d6eb8b2e6e
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-30 12:42:40 +0200

    added test case for user account registration for a supervisor in the namespace

--- tests/api/test_supervisorNS.py
+++ tests/api/test_supervisorNS.py
@@ -1,7 +1,9 @@
 import pytest
 
+from src.database import Supervisor, TravelAgent, Customer, Country, Activity, User, db
 
 from tests.fixtures import app, client, agency
+
 
 def test_add_supervisor(client,agency):
 
@@ -42,4 +44,26 @@ def test_add_supervisor_error(client, agency):
     error = parsed["message"]
 
     assert error == "Please insert your first and last name seperated by a space"
+
+
+def test_register_supervisor(client,agency):
+
+    supervisor = db.session.query(Supervisor).filter_by(employee_id=135).first()
+
+    supervisor_id = supervisor.employee_id
+
+    response = client.post(f"/supervisor/{supervisor_id}/register",json={
+        "username":"Mark",
+        "password":"Hulk"
+    })
+
+    assert response.status_code == 200
+
+    parsed = response.get_json()
+    user_response = parsed["user"]
+
+    assert user_response["id"] == 1
+    assert user_response["username"] == "Mark"
+
+
 
