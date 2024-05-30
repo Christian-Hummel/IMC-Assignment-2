a00f3fcb9e87192c250b1f6cb8615dcddd59baca
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-30 09:41:10 +0200

    added test case for add supervisor error

--- tests/api/test_supervisorNS.py
+++ tests/api/test_supervisorNS.py
@@ -24,3 +24,22 @@ def test_add_supervisor(client,agency):
     assert supervisor_response["address"] == "103 Avenue, 4932 New York"
     assert supervisor_response["salary"] == 20000
     assert supervisor_response["nationality"] == "USA"
+
+
+def test_add_supervisor_error(client, agency):
+
+    response = client.post("/supervisor/", json={
+        "name":"JasonBourne",
+        "address":"Unknown Street 55, 2234 Dallas",
+        "salary": 10000,
+        "nationality": "USA"
+    })
+
+
+    assert response.status_code == 400
+
+    parsed = response.get_json()
+    error = parsed["message"]
+
+    assert error == "Please insert your first and last name seperated by a space"
+
