21c7aeb4581d02c01a4695eeee9fc7ae0627320e
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-05-30 20:34:53 +0200

    added login requirements and methods to fetch current user

--- src/app.py
+++ src/app.py
@@ -1,16 +1,20 @@
 import os
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
+
 from .model.agency import Agency
-from .database import db
+from .database import User,db
 from .api.supervisorNS import supervisor_ns
 
 agency = Agency()
 
 def create_app(database_uri="sqlite:///travelbase.db"):
+
     travelroute_app = Flask(__name__)
     # Configure the database
     travelroute_app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+    travelroute_app.config['JWT_SECRET_KEY'] = "IZGYWPW82P7QY"
 
     # Initialize database on app
     db.init_app(travelroute_app)
@@ -21,6 +25,12 @@ def create_app(database_uri="sqlite:///travelbase.db"):
         with travelroute_app.app_context():
             db.create_all()
 
+    # create Class for handling web tokens
+    jwt = JWTManager()
+
+    #initialize jwt it on app
+    jwt.init_app(travelroute_app)
+
 
     # need to extend this class for custom objects, so that they can be jsonified
     travelroute_api = Api(travelroute_app, title="Hammertrips: An App for booking journeys that you will not forget")
@@ -30,6 +40,17 @@ def create_app(database_uri="sqlite:///travelbase.db"):
     #travelroute_api.add_namespace(travelagent_ns)
     #travelroute_api.add_namespace(customer_ns)
     #travelroute_api.add_namespace(country_ns)
+
+
+    @jwt.user_identity_loader
+    def user_identity_lookup(user):
+        return user.id
+
+
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(id=identity).first()
 
 
     return travelroute_app
